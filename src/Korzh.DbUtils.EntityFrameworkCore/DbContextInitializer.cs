f8022b37306373c3d0ffd420d82546e659fabf1b
Anton Ivanenko <anton_ivanenko@outlook.com>
2019-07-08 13:25:44 +0300

    Add IDataRecordExtentions

--- src/Korzh.DbUtils.EntityFrameworkCore/DbContextInitializer.cs
+++ src/Korzh.DbUtils.EntityFrameworkCore/DbContextInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Linq;
 using System.Collections.Generic;
 
---
+++ src/Korzh.DbUtils.EntityFrameworkCore/IDataRecordExtentions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Korzh.DbUtils.EntityFrameworkCore
+{
+    public static class IDataRecordExtentions
+    {
+
+        public static bool TryGetProperty<T>(this IDataRecord dataRecord, string name, out T value)
+        {
+            var result = TryGetProperty(dataRecord, name, typeof(T),out var valueObj);
+            value = (T)valueObj;
+            return result;
+        }
+             
+        public static bool TryGetProperty(this IDataRecord dataRecord, string name, Type type, out object value)
+        {
+            // To Do move logic from DataRecord here
+            value = null;
+            return false;
+        }
+
+
+    }
+}
