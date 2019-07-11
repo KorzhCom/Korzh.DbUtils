3baf3e9513d80412cbc98b59ec5f34cc21adaff0
Anton Ivanenko <anton_ivanenko@outlook.com>
2019-07-11 17:51:25 +0300

    Add export options and ignore _EFMigrationsHistory table

--- src/Korzh.DbTool/ExportCommand.cs
+++ src/Korzh.DbTool/ExportCommand.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 using System.Data.Common;
 using System.Data;
 using System.Data.SqlClient;
@@ -18,6 +18,35 @@ namespace Korzh.DbTool
 {
     public class ExportCommand : ICommand
     {
+
+        public class ArgumentsAndOptions
+        {
+
+            public readonly CommandArgument ConnectionIdArg;
+
+            public readonly CommandOption OutputPathOption;
+
+            public readonly CommandOption ZipOption;
+
+            public ArgumentsAndOptions(CommandLineApplication command)
+            {
+                ConnectionIdArg = command.Argument("<connection ID>", "The ID of some previously registered connection")
+                                           .IsRequired();
+
+                OutputPathOption = command.Option("--output|-o:<OUTPUT_DIRECTORY>", "Directory in which to place the export files", CommandOptionType.SingleOrNoValue)
+                                           .Accepts(config => config.LegalFilePath());
+
+                ZipOption = command.Option("--zip:<ZIP_NAME>", "Pack result to zip package", CommandOptionType.SingleOrNoValue);
+            }
+
+            public string ConnectionId => ConnectionIdArg.Value;
+
+            public string OutputPath => OutputPathOption.HasValue() ? OutputPathOption.Value() : ".";
+
+            public string PackToZip => ZipOption.HasValue() ? ZipOption.Value() : null;
+
+        }
+
         public static void Configure(CommandLineApplication command, GlobalOptions options)
         {
             command.Description = "Exports some DB to a backup archive in the specified format (XML, JSON)";
@@ -26,22 +55,21 @@ namespace Korzh.DbTool
             command.Options.Add(options.FormatOption);
             command.Options.Add(options.LocalConfigFilePathOption);
 
-            var connectionArgument = command.Argument("<connection ID>", "The ID of some previously registered connection")
-                                            .IsRequired();
+            var arguments = new ArgumentsAndOptions(command);
 
-            command.OnExecute(new ExportCommand(connectionArgument, options).Run);
+            command.OnExecute(new ExportCommand(arguments, options).Run);
 
         }
 
-        private readonly CommandArgument _connectionIdArg;
+        private readonly ArgumentsAndOptions _arguments;
         private readonly GlobalOptions _options;
 
         private DbConnection _connection;
 
-        public ExportCommand(CommandArgument connectionIdArg, GlobalOptions options)
+        public ExportCommand(ArgumentsAndOptions arguments, GlobalOptions options)
         {
             _options = options;
-            _connectionIdArg = connectionIdArg;
+            _arguments = arguments;
         }
 
         private void InitConnection(ConnectionInfo info)
@@ -89,18 +117,36 @@ namespace Korzh.DbTool
 
         private IDataPacker GetPacker()
         {
-            var zipFilePath = "export.zip";
-            return new ZipFilePacker(zipFilePath);
+            if (_arguments.ZipOption.HasValue()) {
+                string zipFileName = _arguments.PackToZip;
+                if (zipFileName != null && !zipFileName.EndsWith(".zip")) {
+                    zipFileName += ".zip";
+                }
+
+                var zipFilePath = Path.Combine(_arguments.OutputPath, zipFileName ?? _arguments.ConnectionId + "_" 
+                                              + DateTime.Now.ToString("yyyy-MM-dd") + ".zip");
+
+                return new ZipFilePacker(zipFilePath);
+            }
+            else if (_arguments.OutputPathOption.HasValue()) {
+                Directory.CreateDirectory(_arguments.OutputPath);
+                return new FileFolderPacker(_arguments.OutputPath);
+            }
+
+            var directory = _arguments.ConnectionId + "_" + DateTime.Now.ToString("yyyy-MM-dd");
+
+            Directory.CreateDirectory(directory);
+
+            return new FileFolderPacker(directory);
         }
 
         public int Run()
         {
 
-            var connectionId = _connectionIdArg.Value;
             var storage = new ConnectionStorage(_options.ConfigFilePath);
-            var info = storage.Get(connectionId);
+            var info = storage.Get(_arguments.ConnectionId);
             if (info == null) {
-                Console.WriteLine("Connection with current ID is not found: " + connectionId);
+                Console.WriteLine("Connection with current ID is not found: " + _arguments.ConnectionId);
                 return -1;
             }
 
