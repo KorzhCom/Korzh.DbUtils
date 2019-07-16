13247b2e4c4d0333e6a3612791ebfb643c72860b
Sergiy Korzh <sergey.korzh@live.com>
2019-07-16 11:48:56 +0300

    XML Docs for DataRecord.cs

--- src/Korzh.DbUtils.Import/DataRecord.cs
+++ src/Korzh.DbUtils.Import/DataRecord.cs
@@ -8,7 +8,11 @@ using System.Text;
 
 namespace Korzh.DbUtils
 {
-
+    /// <summary>
+    /// Represents one record in a dataset.
+    /// Implements the <see cref="System.Data.IDataRecord" />
+    /// </summary>
+    /// <seealso cref="System.Data.IDataRecord" />
     public class DataRecord : IDataRecord
     {
 
@@ -16,17 +20,23 @@ namespace Korzh.DbUtils
 
         private readonly List<string> _keys = new List<string>();
 
+        /// <summary>
+        /// Gets the number of columns in the current row.
+        /// </summary>
+        /// <value>The field count.</value>
         public int FieldCount => _properties.Count;
 
-        public object this[string name]
-        {
-            get 
-            {
+        /// <summary>
+        /// Gets or sets the value of the column by its name.
+        /// </summary>
+        /// <param name="name">The column name.</param>
+        /// <returns>System.Object.</returns>
+        public object this[string name] {
+            get  {
                 return _properties[name];
             }
 
-            set 
-            {
+            set  {
                 _properties[name] = value;
                 if (!_keys.Contains(name)) {
                     _keys.Add(name);
@@ -34,19 +44,43 @@ namespace Korzh.DbUtils
             }
         }
 
+        /// <summary>
+        /// Gets the value of the column by its index.
+        /// </summary>
+        /// <param name="i">The index of the column in the row.</param>
+        /// <returns>System.Object.</returns>
         public object this[int i] => _properties[_keys[i]];
 
 
+        /// <summary>
+        /// Gets the value of the specified column as a Boolean.
+        /// </summary>
+        /// <param name="i">The zero-based column ordinal.</param>
+        /// <returns>The value of the column.</returns>
         public bool GetBoolean(int i)
         {
             return (bool)GetValue(i);
         }
 
+        /// <summary>
+        /// Gets the 8-bit unsigned integer value of the specified column.
+        /// </summary>
+        /// <param name="i">The zero-based column ordinal.</param>
+        /// <returns>The 8-bit unsigned integer value of the specified column.</returns>
         public byte GetByte(int i)
         {
             return (byte)GetValue(i);
         }
 
+        /// <summary>
+        /// Reads a stream of bytes from the specified column offset into the buffer as an array, starting at the given buffer offset.
+        /// </summary>
+        /// <param name="i">The zero-based column ordinal.</param>
+        /// <param name="fieldOffset">The index within the field from which to start the read operation.</param>
+        /// <param name="buffer">The buffer into which to read the stream of bytes.</param>
+        /// <param name="bufferoffset">The index for buffer to start the read operation.</param>
+        /// <param name="length">The number of bytes to read.</param>
+        /// <returns>The actual number of bytes read.</returns>
         public long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length)
         {
             var value = GetValue(i);
@@ -69,11 +103,25 @@ namespace Korzh.DbUtils
             return 0;
         }
 
+        /// <summary>
+        /// Gets the character value of the specified column.
+        /// </summary>
+        /// <param name="i">The zero-based column ordinal.</param>
+        /// <returns>The character value of the specified column.</returns>
         public char GetChar(int i)
         {
             return (char)GetValue(i);
         }
 
+        /// <summary>
+        /// Reads a stream of characters from the specified column offset into the buffer as an array, starting at the given buffer offset.
+        /// </summary>
+        /// <param name="i">The zero-based column ordinal.</param>
+        /// <param name="fieldoffset">The index within the row from which to start the read operation.</param>
+        /// <param name="buffer">The buffer into which to read the stream of bytes.</param>
+        /// <param name="bufferoffset">The index for buffer to start the read operation.</param>
+        /// <param name="length">The number of bytes to read.</param>
+        /// <returns>The actual number of characters read.</returns>
         public long GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length)
         {
             var value = GetString(i);
@@ -88,86 +136,175 @@ namespace Korzh.DbUtils
             return 0;
         }
 
+        /// <summary>
+        /// Returns an <see cref="T:System.Data.IDataReader"></see> for the specified column ordinal.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The <see cref="T:System.Data.IDataReader"></see> for the specified column ordinal.</returns>
+        /// <remarks>NOT IMPLEMENTED IN DataRecord CLASS. DO NOT USE!</remarks>
+        /// <exception cref="NotImplementedException"></exception>
         public IDataReader GetData(int i)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Gets the data type information for the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The data type information for the specified field.</returns>
         public string GetDataTypeName(int i)
         {
             return GetFieldType(i).ToString();
         }
 
+        /// <summary>
+        /// Gets the date and time data value of the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The date and time data value of the specified field.</returns>
         public DateTime GetDateTime(int i)
         {
             return (DateTime)GetValue(i);
         }
 
+        /// <summary>
+        /// Gets the fixed-position numeric value of the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The fixed-position numeric value of the specified field.</returns>
         public decimal GetDecimal(int i)
         {
             return (decimal)GetValue(i);
         }
 
+        /// <summary>
+        /// Gets the double-precision floating point number of the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The double-precision floating point number of the specified field.</returns>
         public double GetDouble(int i)
         {
             return (double)GetValue(i);
         }
 
+        /// <summary>
+        /// Gets the <see cref="T:System.Type"></see> information corresponding to the type of <see cref="T:System.Object"></see> that would be returned from <see cref="M:System.Data.IDataRecord.GetValue(System.Int32)"></see>.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The <see cref="T:System.Type"></see> information corresponding to the type of <see cref="T:System.Object"></see> that would be returned from <see cref="M:System.Data.IDataRecord.GetValue(System.Int32)"></see>.</returns>
         public Type GetFieldType(int i)
         {
             return GetValue(i).GetType();
         }
 
+        /// <summary>
+        /// Gets the single-precision floating point number of the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The single-precision floating point number of the specified field.</returns>
         public float GetFloat(int i)
         {
             return (float)GetValue(i);
         }
 
+        /// <summary>
+        /// Returns the GUID value of the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The GUID value of the specified field.</returns>
         public Guid GetGuid(int i)
         {
             return (Guid)GetValue(i);
         }
 
+        /// <summary>
+        /// Gets the 16-bit signed integer value of the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The 16-bit signed integer value of the specified field.</returns>
         public short GetInt16(int i)
         {
             return (short)GetValue(i);
         }
 
+        /// <summary>
+        /// Gets the 32-bit signed integer value of the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The 32-bit signed integer value of the specified field.</returns>
         public int GetInt32(int i)
         {
             return (int)GetValue(i);
         }
 
+        /// <summary>
+        /// Gets the 64-bit signed integer value of the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The 64-bit signed integer value of the specified field.</returns>
         public long GetInt64(int i)
         {
             return (long)GetValue(i);
         }
 
+        /// <summary>
+        /// Gets the name for the field to find.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The name of the field or the empty string (""), if there is no value to return.</returns>
         public string GetName(int i)
         {
             return _keys[i];
         }
 
+        /// <summary>
+        /// Return the index of the named field.
+        /// </summary>
+        /// <param name="name">The name of the field to find.</param>
+        /// <returns>The index of the named field.</returns>
         public int GetOrdinal(string name)
         {
             return _keys.IndexOf(name);
         }
 
+        /// <summary>
+        /// Gets the string value of the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The string value of the specified field.</returns>
         public string GetString(int i)
         {
             return (string)GetValue(i);
         }
 
+        /// <summary>
+        /// Return the value of the specified field.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>The <see cref="T:System.Object"></see> which will contain the field value upon return.</returns>
         public object GetValue(int i)
         {
             return _properties[_keys[i]];
         }
 
+        /// <summary>
+        /// Populates an array of objects with the column values of the current record.
+        /// </summary>
+        /// <param name="values">An array of <see cref="T:System.Object"></see> to copy the attribute fields into.</param>
+        /// <returns>The number of instances of <see cref="T:System.Object"></see> in the array.</returns>
+        /// <exception cref="NotImplementedException"></exception>
         public int GetValues(object[] values)
         {
-            throw new NotImplementedException();
+            _properties.Values.CopyTo(values, 0);
+            return _properties.Count;
         }
 
+        /// <summary>
+        /// Return whether the specified field is set to null.
+        /// </summary>
+        /// <param name="i">The index of the field to find.</param>
+        /// <returns>true if the specified field is set to null; otherwise, false.</returns>
         public bool IsDBNull(int i)
         {
             var value = GetValue(i);
