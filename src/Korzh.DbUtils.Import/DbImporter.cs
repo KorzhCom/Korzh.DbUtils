b6518f3170c9ef14bbb6acd7cecbe1371f7d0075
Anton Ivanenko <anton_ivanenko@outlook.com>
2019-08-26 16:23:10 +0300

    Fix StartSeeding parameter

--- src/Korzh.DbUtils.Import/DbImporter.cs
+++ src/Korzh.DbUtils.Import/DbImporter.cs
@@ -39,11 +39,12 @@ namespace Korzh.DbUtils.Import
             var datasets = _dbWriter.GetDatasets();
             try {
                 foreach (var table in datasets) {
-                    _dbWriter.StartSeeding(table);
                     try {
                         using (var datasetStream = _dataUnpacker.OpenStreamForUnpacking(table.Name)) {
                             if (datasetStream != null) {
                                 var dataset = _datasetImporter.StartImport(datasetStream);
+                                _dbWriter.StartSeeding(dataset);
+
                                 while (_datasetImporter.HasRecords()) {
                                     try {
                                         _dbWriter.WriteRecord(_datasetImporter.NextRecord());
